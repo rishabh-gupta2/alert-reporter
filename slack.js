36fa1687b4965d4ab92142dbb301f249a48695e8
Rishabh Gupta <rishabh.gupta@razorpay.com>
2022-07-06 14:49:33 +0530

    initial slack setup

--- slack.js
+++ slack.js
@@ -3,6 +3,49 @@ const axios = require('axios');
 const WEBHOOK_ID = ""
 const APP_TOKEN = ""
 
+const reportPayload = {
+    blocks: [
+        {
+            "type": "header",
+            "text": {
+                "type": "plain_text",
+                "text": "Edge Performance Report"
+            }
+        },
+        {
+            "type": "divider"
+        },
+        {
+            type: "section",
+            text: {
+                type: "mrkdwn",
+                text: ""
+            }
+        },
+        {
+            "type": "divider"
+        },
+        {
+            type: "section",
+            text: {
+                type: "mrkdwn",
+                text: ""
+            }
+        },
+        {
+            "type": "divider"
+        },
+        {
+            type: "section",
+            text: {
+                type: "mrkdwn",
+                text: ""
+            }
+        }
+    ]
+};
+
+
 export function sendReport() {
 }
 
